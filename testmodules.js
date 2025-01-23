249f20111dbe08f7ee23b5ef49064815a8c7ba12
Aryan Saxena <aryansaxena@Aryans-MacBook-Pro.local>
2025-01-23 18:50:14 +0530

    modules and globals

---
+++ testmodules.js
@@ -0,0 +1,8 @@
+const names = require("./modules");
+const func = require("./testmodules2");
+
+console.log(names);
+
+names.sayHello(names.a);
+
+func();
---
+++ testmodules2.js
@@ -0,0 +1,5 @@
+const func = ()=>{
+    console.log("Hello World");
+}
+
+module.exports = func;
